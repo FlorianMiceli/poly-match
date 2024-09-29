793ab3855279b1e411103b2d346e721c90415bb7
FlorianMiceli <florian.miceli@universite-paris-saclay.fr>
2024-09-29 15:43:47 +0200

    click on song : preview + spotify link

--- frontend/src/components/Song.vue
+++ frontend/src/components/Song.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 import { getProfile, updateProfile } from '@/helpers/userQueriesHelpers';
 import { SpotifyTrack } from '@/types/global_types';
-import { CircleX } from 'lucide-vue-next';
-import { Loader2 } from 'lucide-vue-next'
+import { CircleX, Loader2, ArrowUpRight } from 'lucide-vue-next';
+import { DrawerTrigger } from "vaul-vue";
+
 
 
 const props = defineProps<{
@@ -21,28 +22,56 @@ const removeSong = (song: SpotifyTrack) => {
     })
 }
 
-
+const openSpotify = () => {
+    window.open(props.song.external_urls.spotify, '_blank')
+}
 </script>
 <template>
-    <Badge class="flex items-center space-x-2 p-1" variant="outline">
 
-        <!-- Song Cover -->
-        <Avatar class="w-8 h-8 ml-1">
-            <AvatarImage :src="song.album.images[song.album.images.length - 1].url || ''" alt="Song Cover" />
-            <AvatarFallback>
-                <Loader2 class="w-4 h-4 animate-spin" />
-            </AvatarFallback>
-        </Avatar>
 
-        <!-- Song Title and Artist -->
-        <div class="flex flex-col">
-            <span class="text-sm" :class="{ 'pr-2': !removeable }">{{ song.name }}</span>
-            <span class="text-xs text-gray-500">{{ song.artists[0].name }}</span>
-        </div>
+    <Drawer>
+        <DrawerTrigger as-child> 
+            <Badge class="flex items-center space-x-2 p-1 cursor-pointer" variant="outline">
+                <!-- Song Cover -->
+                <Avatar class="w-8 h-8 ml-1">
+                    <AvatarImage :src="song.album.images[0].url || ''" alt="Song Cover" />
+                    <AvatarFallback>
+                        <Loader2 class="w-4 h-4 animate-spin" />
+                    </AvatarFallback>
+                </Avatar>
 
-        <!-- Button to remove the song from the list -->
-        <Button v-if="removeable" size="icon" variant="ghost" @click="removeSong(song)" >
-            <CircleX class="w-6 h-6" />
-        </Button>
-    </Badge>
+                <!-- Song Title and Artist -->
+                <div class="flex flex-col">
+                    <span class="text-sm" :class="{ 'pr-2': !removeable }">{{ song.name }}</span>
+                    <span class="text-xs text-gray-500">{{ song.artists[0].name }}</span>
+                </div>
+
+                <!-- Button to remove the song from the list -->
+                <Button v-if="removeable" size="icon" variant="ghost" @click="removeSong(song)">
+                    <CircleX class="w-6 h-6" />
+                </Button>
+            </Badge>
+        </DrawerTrigger>
+        <DrawerContent>
+            <DrawerHeader>
+                <DrawerTitle>
+                    <span class="font-bold text-xl">{{ song.name }}</span>
+                    <span class="text-gray-400 text-lg block">{{ song.artists[0].name }}</span>
+                </DrawerTitle>
+            </DrawerHeader>
+            <div class="px-4 flex flex-col items-center">
+                <img :src="song.album.images[0].url" :alt="song.album.name" class="rounded-lg">
+                <audio
+                    autoplay
+                    ref="audio"
+                    :src="song.preview_url"
+                    class="hidden"
+                />
+                <Button class="my-4" @click="openSpotify()">
+                    <ArrowUpRight />
+                    Ecouter sur Spotify
+                </Button>
+            </div>
+    </DrawerContent>
+  </Drawer>
 </template>
