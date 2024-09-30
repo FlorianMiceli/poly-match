631b62a264cfe671de211fdd60a4237fa90e0ba6
FlorianMiceli <florian.miceli@universite-paris-saclay.fr>
2024-09-30 16:49:11 +0200

    try updating backend url

--- backend/app.js
+++ backend/app.js
@@ -32,7 +32,7 @@ app.get("/api-docs.json", (req, res) => {
 });
 
 // Enable CORS
-const origin = process.env.NODE_ENV === "production" ? "https://poly-match-frontend.vercel.app/" : "http://localhost:" + FRONTEND_PORT;
+const origin = process.env.NODE_ENV === "production" ? "https://poly-match-backend.vercel.app/" : "http://localhost:" + FRONTEND_PORT;
 // restrict to only allow requests from the frontend
 app.use(
     cors(
